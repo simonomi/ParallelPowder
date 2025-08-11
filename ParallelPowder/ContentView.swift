541a50f18c83bd6da36b9f7833f17b8d13039816
alice pellerin <thealicepellerin@gmail.com>
2025-08-11 16:35:39 -0400

    Initial Commit

---
+++ ParallelPowder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ParallelPowder
+//
+//  Created by alice pellerin on 8/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ParallelPowder/ParallelPowderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParallelPowderApp.swift
+//  ParallelPowder
+//
+//  Created by alice pellerin on 8/11/25.
+//
+
+import SwiftUI
+
+@main
+struct ParallelPowderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
